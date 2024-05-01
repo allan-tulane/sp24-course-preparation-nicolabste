b36bb6fed675b6cea1d2d95fa5cb2519c52ec466
nicolabste <126182691+nicolabste@users.noreply.github.com>
2024-05-01 05:06:14 +0000

    commit

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ CMPS 2200  Preparation
 """
 
 def myfunction(input):
-	""" done. """
-	return input
+  """ done. """
+  return input
+print(myfunction('Hello World'))
 
 
 
--- test_main.py
+++ test_main.py
@@ -1,10 +1,7 @@
 from main import *
 
-def test_one():
-	""" done. """
-	assert myfunction('Hello') == 'Hello'
-	assert myfunction(1) == 1
-	assert myfunction(6) == -1
-
-def test_two():
-	assert myfunction(3+4) == 7
+def test_myfunction():
+  """ done. """
+  assert myfunction('Hello') == 'Hello'
+  assert myfunction(1) == 1
+  assert myfunction(6) == 6
